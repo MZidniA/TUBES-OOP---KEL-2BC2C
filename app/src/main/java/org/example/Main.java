905af72c5818aac2f9826431ab33ff75037d0b13
Inggrieda <163884726+Inggrieda@users.noreply.github.com>
2025-05-07 12:44:28 +0700

    Update Main.java

--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -7,4 +7,8 @@ public class Main {
     public String getGreeting() {
         return "Hello World!";
     }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
 }
