e2db320cc9237ff6e9e784b154f2561da762510d
Inggrieda <163884726+Inggrieda@users.noreply.github.com>
2025-05-31 20:37:54 +0700

    items fixed

--- app/src/main/java/org/example/model/Items/Crops.java
+++ app/src/main/java/org/example/model/Items/Crops.java
@@ -1,5 +1,11 @@
 package org.example.model.Items;
 
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.example.controller.UtilityTool;
+
 public class Crops extends Items implements EdibleItem {
     private int jumlahcropperpanen;
     private int energyRestored; 
@@ -16,6 +22,67 @@ public class Crops extends Items implements EdibleItem {
 
     public void setJumlahcropperpanen(int jumlahcropperpanen) {
         this.jumlahcropperpanen = jumlahcropperpanen;
+    }
+
+    public void loadImage() {
+        String path = null;
+        String lowerName = getName().toLowerCase().replace(" ", "");
+
+        switch (lowerName) {
+            case "parsnip":
+                path = "/crops/Parsnip.png";
+                break;
+            case "cauliflower":
+                path = "/crops/Cauliflower.png";
+                break;
+            case "potato":
+                path = "/crops/Potato.png";
+                break;
+            case "wheat":
+                path = "/crops/Wheat.png";
+                break;
+            case "blueberry":
+                path = "/crops/Blueberry.png";
+                break;
+            case "tomato":
+                path = "/crops/Tomato.png";
+                break;
+            case "hotpepper":
+                path = "/crops/Hot_Pepper.png";
+                break;
+            case "melon":
+                path = "/crops/Melon.png";
+                break;
+            case "cranberry":
+                path = "/crops/Cranberry.png";
+                break;
+            case "pumpkin":
+                path = "/crops/Pumpkin.png";
+                break;
+            case "grape":
+                path = "/crops/Grape.png";
+                break;
+            default:
+                this.image = null;
+                System.err.println("No image available for crop: " + getName());
+                return;
+        }
+
+        try {
+            this.image = ImageIO.read(getClass().getResourceAsStream(path));
+            if (this.image == null) {
+                System.err.println("Failed to load image for " + getName() + " at path: " + path);
+            } else {
+                UtilityTool uTool = new UtilityTool();
+                this.image = uTool.scaleImage(this.image, 32, 32);
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading image for " + getName() + " from path " + path + ": " + e.getMessage());
+            this.image = null;
+        } catch (IllegalArgumentException e) {
+            System.err.println("Invalid image path for " + getName() + ": " + e.getMessage());
+            this.image = null;
+        }
     }
 
     @Override
--- app/src/main/java/org/example/model/Items/Fish.java
+++ app/src/main/java/org/example/model/Items/Fish.java
@@ -1,8 +1,12 @@
 package org.example.model.Items;
 
+import java.io.IOException;
 import java.util.EnumSet;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
+import org.example.controller.UtilityTool;
 import org.example.model.GameTime;
 import org.example.model.enums.FishType;
 import org.example.model.enums.LocationType;
@@ -63,6 +67,91 @@ public class Fish extends Items implements EdibleItem {
 
     public void setTimeRanges(List<GameTime> timeRanges) {
         this.timeRanges = timeRanges;
+    }
+
+    public void loadImage() {
+        String path = null;
+        String lowerName = getName().toLowerCase().replace(" ", "");
+
+        switch (lowerName) {
+            case "bullhead":
+                path = "/fish/Bullhead.png";
+                break;
+            case "carp":
+                path = "/fish/Carp.png";
+                break;
+            case "chub":
+                path = "/fish/Chub.png";
+                break;
+            case "largemouthbass":
+                path = "/fish/LargemouthBass.png";
+                break;
+            case "rainbowtrout":
+                path = "/fish/RainbowTrout.png";
+                break;
+            case "sturgeon":
+                path = "/fish/Sturgeon.png";
+                break;
+            case "midnightcarp":
+                path = "/fish/MidnightCarp.png";
+                break;
+            case "flounder":
+                path = "/fish/Flounder.png";
+                break;
+            case "halibut":
+                path = "/fish/Halibut.png";
+                break;
+            case "octopus":
+                path = "/fish/Octopus.png";
+                break;
+            case "pufferfish":
+                path = "/fish/Pufferfish.png";
+                break;
+            case "sardine":
+                path = "/fish/Sardine.png";
+                break;
+            case "supercucumber":
+                path = "/fish/SuperCucumber.png";
+                break;
+            case "catfish":
+                path = "/fish/Catfish.png";
+                break;
+            case "salmon":
+                path = "/fish/Salmon.png";
+                break;
+            case "angler":
+                path = "/fish/Angler.png";
+                break;
+            case "crimsonfish":
+                path = "/fish/Crimsonfish.png";
+                break;
+            case "glacierfish":
+                path = "/fish/Glacierfish.png";
+                break;
+            case "legend":
+                path = "/fish/Legend.png";
+                break;
+            default:
+                this.image = null;
+                System.err.println("No image available for fish: " + getName());
+                return;
+        }
+
+        try {
+            this.image = ImageIO.read(getClass().getResourceAsStream(path));
+            if (this.image == null) {
+                System.err.println("Failed to load image for " + getName() + " at path: " + path);
+            } else {
+                UtilityTool uTool = new UtilityTool();
+                this.image = uTool.scaleImage(this.image, 32, 32);
+            }
+        } catch (IOException e) {
+            System.err.println("Error loading image for " + getName() + " from path " + path + ": " + e.getMessage());
+            this.image = null;
+        } catch (IllegalArgumentException e) {
+            System.err.println("Invalid image path for " + getName() + ": " + e.getMessage());
+            this.image = null;
+        }
     }
 
     @Override
